58dad1f9343b58dd9065012cc1b0ed1c0a0f5545
akashraja1108 <akashraja1108@gmail.com>
2024-07-14 17:08:09 +0000

    Add input and style changes
    Created input element for new file/Folder
    modified the styles

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,38 +1,80 @@
-import { useState } from 'react'
-import { data } from '../utils/data.js'
+import { useState } from 'react';
+import { data as item } from '../utils/data';
 
 const Folder = () => {
-    return (
-        <div>
-            {data.map((item,i) =>
-                <SubFolder item={item} key={i}/>
-            )}
-        </div>
-    )
-}
+  const [data, setData] = useState(item);
+
+  return (
+    <>
+      {data?.map((item, i) => (
+        <SubFolder item={item} key={i} />
+      ))}
+    </>
+  );
+};
 
 const SubFolder = ({ item }) => {
+  const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    isVisible: false,
+    isFolder: null,
+  });
 
-    const [expand, setExpand] = useState(false)
+  const handleButtonClick = isFolder => {
+    setExpand(true);
+    setShowInput({ isVisible: true, isFolder: isFolder });
+  };
 
-    const handleClick = () => {
-        setExpand(!expand)
-    }
+  const handleInputEnter = e => {
+    if (e.keyCode !== 13) return;
+    const { value } = e.target;
+    console.log(value);
+    setShowInput({ isFolder: null, isVisible: false });
+  };
 
+  if (item.isFolder) {
     return (
-        <div>
-            {item.isFolder && <span style={{ color: 'red', cursor: 'pointer' }} onClick={() => handleClick()}>+</span>}
+      <div className="container">
+        <div className="folder">
+          <span
+            onClick={() => setExpand(!expand)}
+            style={{ cursor: 'pointer' }}
+          >
+            {!expand ? '📁 ' : '📂 '}
             {item.name}
-            {item.isFolder && expand &&
-                (
-                    <div style={{ marginLeft: 20}}>
-                        {item.children.map((child, index) => (
-                            <SubFolder key={index} item={child} />
-                        ))}
-                    </div>
-                )}
+          </span>
+          <div className="buttons">
+            <button onClick={e => handleButtonClick(true)}>+ Folder</button>
+            <button onClick={e => handleButtonClick(false)}>+ File</button>
+          </div>
         </div>
-    )
-}
+        {expand && (
+          <div className="items">
+            {showInput.isVisible && (
+              <span className="input">
+                {showInput.isFolder ? '📁 ' : '📄 '}
+                <input
+                  type="text"
+                  autoFocus
+                  onBlur={() =>
+                    setShowInput({ ...setShowInput, isVisible: false })
+                  }
+                  onKeyDown={handleInputEnter}
+                />
+              </span>
+            )}
+            {item.children.map((item, i) => (
+              <div key={i} style={{ marginLeft: 30 }}>
+                <SubFolder item={item} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return <div className="file">📄 {item.name}</div>;
+  }
+};
 
-export default Folder
+export default Folder;
